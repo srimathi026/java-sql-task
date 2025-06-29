de375f6ecd561628369917e69738e5d59c3d7496
srimathi026 <srimathi160002@gmail.com>
2025-06-29 05:08:03 -0700

    Create task4.java

---
+++ day5/task4.java
@@ -0,0 +1,17 @@
+package method;
+
+public class Main extends Thread {
+	public void run() {
+		for(int i=1;i<=10;i++) {
+		System.out.println(i);
+	}
+	}
+}
+class Execute{
+	public static void main(String[] args) {
+		Main m=new Main();
+		m.start();
+
+	
+	}
+}
